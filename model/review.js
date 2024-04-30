62a1c81c267c90ffdb2883fd03acc009128948d7
soheilaminlu <soheilaminlo80@gmail.com>
2024-04-30 16:29:05 +0330

    design review and acco model

---
+++ model/review.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+
+
+const reviewSchema = new schema ({
+  owner:{type:mongoose.Types.ObjectId , ref:'User'} , 
+  place:{type:mongoose.Types.ObjectId , ref:'Accommodation'} , 
+  description:{type:String , required:true} ,  
+  rate:{type:Number , min:1 , max:5}
+})
+
+module.exports = mongoose.model('Review' , reviewSchema)
