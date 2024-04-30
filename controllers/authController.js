4e50a8405d4533b1112202bbf8bedf22b9e7ab4b
soheilaminlu <soheilaminlo80@gmail.com>
2024-04-30 13:01:25 +0330

    set signup and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,14 +26,29 @@ module.exports.signupController = async (req, res) => {
   if(!user) {
     return res.status(400).json({message:"Failed to signing Up"})
   }
-  const accessToken = generateAccessToken(user._id , user.email , user.role)
-  const refreshToken = generateRefreshToken(user._id);
+  const accessToken = await generateAccessToken(user._id , user.email , user.role)
+  const refreshToken = await generateRefreshToken(user._id);
+  res.cookie('jwt' , accessToken  , {httpOnly:true})
   console.log(accessToken)
+  console.log(refreshToken)
+  await user.save()
   return res.status(200).json({message:"User Created Successfuly" , user:user , accessToken:accessToken , refreshToken:refreshToken})
 };
 
 module.exports.loginController = async (req, res) => {
-    // Implement login logic here
+    const {email , password} = req.body;
+    const user = await User.findOne({email});
+    if(!user) {
+        return res.status(400).json({message:"please Signup First"})
+    }
+    const passwordMatch = await bcrypt.compare(password , user.password);
+    if(!passwordMatch) {
+        return res.status(400).json({message:"Invalid Password"})
+    }
+    const accessToken = await generateAccessToken(user._id , user.email , user.role)
+  const refreshToken = await generateRefreshToken(user._id);
+  res.cookie('jwt' , accessToken  , {httpOnly:true})
+    return res.status(200).json({message:"Login Successfuly" , accessToken , refreshToken})
 };
 
 module.exports.logoutController = async (req, res) => {
